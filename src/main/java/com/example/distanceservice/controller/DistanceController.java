751a88da8cfb263850e5557bc03b2d10ca5e646e
egoo00 <egooo914@gmail.com>
2025-06-21 00:20:50 +0300

    Update DistanceController.java

--- src/main/java/com/example/distanceservice/controller/DistanceController.java
+++ src/main/java/com/example/distanceservice/controller/DistanceController.java
@@ -2,22 +2,40 @@ package com.example.distanceservice.controller;
 
 import com.example.distanceservice.dto.DistanceResponse;
 import com.example.distanceservice.service.DistanceService;
+import com.example.distanceservice.util.RequestCounter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class DistanceController {
-    private final DistanceService distanceService;
+    private static final String API_PATH_DISTANCE = "/distance";
+    private static final String API_PATH_DISTANCES_BULK = "/distances/bulk";
+    private static final String API_PATH_COUNTER = "/counter";
 
-    
+    private final DistanceService distanceService;
+    private final RequestCounter requestCounter;
+
     @Autowired
-    public DistanceController(DistanceService distanceService) {
+    public DistanceController(DistanceService distanceService, RequestCounter requestCounter) {
         this.distanceService = distanceService;
+        this.requestCounter = requestCounter;
     }
 
-    @GetMapping("/distance")
+    @GetMapping(API_PATH_DISTANCE)
     public DistanceResponse getDistance(@RequestParam String from, @RequestParam String to) {
         return distanceService.calculateDistance(from, to);
+    }
+
+    @PostMapping(API_PATH_DISTANCES_BULK)
+    public List<DistanceResponse> getBulkDistances(@RequestBody List<String[]> cityPairs) {
+        return distanceService.calculateBulkDistances(cityPairs);
+    }
+
+    @GetMapping(API_PATH_COUNTER)
+    public int getRequestCount() {
+        return requestCounter.getCount();
     }
 }
