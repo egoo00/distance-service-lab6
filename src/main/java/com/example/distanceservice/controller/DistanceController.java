a402b061cd96b32ec8d48ff9fc02f1977dbaaef1
egoo00 <egooo914@gmail.com>
2025-06-21 01:10:29 +0300

    Update DistanceController.java

--- src/main/java/com/example/distanceservice/controller/DistanceController.java
+++ src/main/java/com/example/distanceservice/controller/DistanceController.java
@@ -14,6 +14,7 @@ public class DistanceController {
     private static final String API_PATH_DISTANCE = "/distance";
     private static final String API_PATH_DISTANCES_BULK = "/distances/bulk";
     private static final String API_PATH_COUNTER = "/counter";
+    private static final String API_PATH_RESET = "/reset";
 
     private final DistanceService distanceService;
     private final RequestCounter requestCounter;
@@ -26,16 +27,25 @@ public class DistanceController {
 
     @GetMapping(API_PATH_DISTANCE)
     public DistanceResponse getDistance(@RequestParam String from, @RequestParam String to) {
+        requestCounter.increment();
         return distanceService.calculateDistance(from, to);
     }
 
     @PostMapping(API_PATH_DISTANCES_BULK)
     public List<DistanceResponse> getBulkDistances(@RequestBody List<String[]> cityPairs) {
+        requestCounter.increment();
         return distanceService.calculateBulkDistances(cityPairs);
     }
 
     @GetMapping(API_PATH_COUNTER)
     public int getRequestCount() {
+        requestCounter.increment();
         return requestCounter.getCount();
+    }
+
+    @GetMapping(API_PATH_RESET)
+    public void resetCounter() {
+        requestCounter.increment();
+        requestCounter.reset();
     }
 }
