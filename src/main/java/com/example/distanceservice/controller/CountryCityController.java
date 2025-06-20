03db9f06631f9c2f48f8bc3b5bd75cd11e4e8d46
egoo00 <egooo914@gmail.com>
2025-06-21 01:09:33 +0300

    Update CountryCityController.java

--- src/main/java/com/example/distanceservice/controller/CountryCityController.java
+++ src/main/java/com/example/distanceservice/controller/CountryCityController.java
@@ -2,6 +2,7 @@ package com.example.distanceservice.controller;
 
 import com.example.distanceservice.entity.City;
 import com.example.distanceservice.service.CountryCityService;
+import com.example.distanceservice.util.RequestCounter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,19 +16,23 @@ public class CountryCityController {
     private static final String API_PATH_CITIES_BY_COUNTRY_NATIVE = "/api/cities-by-country-native";
 
     private final CountryCityService countryCityService;
+    private final RequestCounter requestCounter;
 
     @Autowired
-    public CountryCityController(CountryCityService countryCityService) {
+    public CountryCityController(CountryCityService countryCityService, RequestCounter requestCounter) {
         this.countryCityService = countryCityService;
+        this.requestCounter = requestCounter;
     }
 
     @GetMapping(API_PATH_CITIES_BY_COUNTRY)
     public List<City> getCitiesByCountry(@RequestParam String countryName) {
+        requestCounter.increment();
         return countryCityService.getCitiesByCountry(countryName);
     }
 
     @GetMapping(API_PATH_CITIES_BY_COUNTRY_NATIVE)
     public List<City> getCitiesByCountryNative(@RequestParam String countryName) {
+        requestCounter.increment();
         return countryCityService.getCitiesByCountryNative(countryName);
     }
 }
