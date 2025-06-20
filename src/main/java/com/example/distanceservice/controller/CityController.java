ca54be7812894b49484ac492a60ac7432778d29f
egoo00 <egooo914@gmail.com>
2025-06-21 00:50:14 +0300

    Update CityController.java

--- src/main/java/com/example/distanceservice/controller/CityController.java
+++ src/main/java/com/example/distanceservice/controller/CityController.java
@@ -2,6 +2,8 @@ package com.example.distanceservice.controller;
 
 import com.example.distanceservice.entity.City;
 import com.example.distanceservice.service.CityService;
+import com.example.distanceservice.util.RequestCounter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,28 +15,35 @@ public class CityController {
     private static final String API_PATH = "/api/cities";
 
     private final CityService cityService;
+    private final RequestCounter requestCounter;
 
-    public CityController(CityService cityService) {
+    @Autowired
+    public CityController(CityService cityService, RequestCounter requestCounter) {
         this.cityService = cityService;
+        this.requestCounter = requestCounter;
     }
 
     @GetMapping
     public List<City> getAllCities() {
+        requestCounter.increment();
         return cityService.getAllCities();
     }
 
     @GetMapping("/{id}")
     public Optional<City> getCityById(@PathVariable Long id) {
+        requestCounter.increment();
         return cityService.getCityById(id);
     }
 
     @PostMapping
     public City saveCity(@RequestBody City city) {
+        requestCounter.increment();
         return cityService.saveCity(city);
     }
 
     @DeleteMapping("/{id}")
     public void deleteCity(@PathVariable Long id) {
+        requestCounter.increment();
         cityService.deleteCity(id);
     }
 }
