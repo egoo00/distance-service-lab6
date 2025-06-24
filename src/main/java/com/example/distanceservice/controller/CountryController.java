1047071f426f9c4e5f5a282b0bfbe326c1ce5413
egoo00 <egooo914@gmail.com>
2025-06-24 16:47:12 +0300

    Update CountryController.java

--- src/main/java/com/example/distanceservice/controller/CountryController.java
+++ src/main/java/com/example/distanceservice/controller/CountryController.java
@@ -2,7 +2,6 @@ package com.example.distanceservice.controller;
 
 import com.example.distanceservice.entity.Country;
 import com.example.distanceservice.service.CountryService;
-import com.example.distanceservice.util.RequestCounter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,61 +14,44 @@ public class CountryController {
     private static final String API_PATH = "/api/countries";
 
     private final CountryService countryService;
-    private final RequestCounter requestCounter;
 
     @Autowired
-    public CountryController(CountryService countryService, RequestCounter requestCounter) {
+    public CountryController(CountryService countryService) {
         this.countryService = countryService;
-        this.requestCounter = requestCounter;
     }
 
     @GetMapping
     public List<Country> getAllCountries() {
-        requestCounter.incrementTotal();
         try {
-            List<Country> countries = countryService.getAllCountries();
-            requestCounter.incrementSuccessful();
-            return countries;
+            return countryService.getAllCountries();
         } catch (Exception e) {
-            requestCounter.incrementFailed();
             throw e;
         }
     }
 
     @GetMapping("/{id}")
     public Optional<Country> getCountryById(@PathVariable Long id) {
-        requestCounter.incrementTotal();
         try {
-            Optional<Country> country = countryService.getCountryById(id);
-            requestCounter.incrementSuccessful();
-            return country;
+            return countryService.getCountryById(id);
         } catch (Exception e) {
-            requestCounter.incrementFailed();
             throw e;
         }
     }
 
     @PostMapping
     public Country saveCountry(@RequestBody Country country) {
-        requestCounter.incrementTotal();
         try {
-            Country savedCountry = countryService.saveCountry(country);
-            requestCounter.incrementSuccessful();
-            return savedCountry;
+            return countryService.saveCountry(country);
         } catch (Exception e) {
-            requestCounter.incrementFailed();
             throw e;
         }
     }
 
     @DeleteMapping("/{id}")
     public void deleteCountry(@PathVariable Long id) {
-        requestCounter.incrementTotal();
         try {
             countryService.deleteCountry(id);
-            requestCounter.incrementSuccessful();
         } catch (Exception e) {
-            requestCounter.incrementFailed();
             throw e;
         }
     }
