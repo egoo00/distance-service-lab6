845b6ec549c91203340201b607b9e99aec3bf556
egoo00 <egooo914@gmail.com>
2025-06-24 17:58:03 +0300

    Update City.java

--- src/main/java/com/example/distanceservice/entity/City.java
+++ src/main/java/com/example/distanceservice/entity/City.java
@@ -1,10 +1,12 @@
 package com.example.distanceservice.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -15,4 +17,18 @@ public class City {
     private String name;
     private double latitude;
     private double longitude;
+
+    @ManyToOne
+    private Country country;
+
+    @OneToMany(mappedBy = "city1", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<CityPair> cityPairs = new ArrayList<>();
+
+    public City(String name, double latitude, double longitude, Country country, List<CityPair> cityPairs) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.country = country;
+        this.cityPairs = cityPairs != null ? cityPairs : new ArrayList<>();
+    }
 }
