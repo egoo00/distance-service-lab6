f6fabe72860a43b805bfa2f9da379980aea754d0
egoo00 <egooo914@gmail.com>
2025-06-24 15:39:27 +0300

    Update LoggingAspect.java

--- src/main/java/com/example/distanceservice/aspect/LoggingAspect.java
+++ src/main/java/com/example/distanceservice/aspect/LoggingAspect.java
@@ -3,8 +3,8 @@ package com.example.distanceservice.aspect;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -17,16 +17,16 @@ public class LoggingAspect {
 
     @Before("execution(* " + CONTROLLER_PACKAGE + ")")
     public void logBefore(JoinPoint joinPoint) {
-        LOGGER.info("Entering method: " + joinPoint.getSignature().getName());
+        LOGGER.info("Вход в метод: " + joinPoint.getSignature().getName());
     }
 
     @AfterReturning(pointcut = "execution(* " + CONTROLLER_PACKAGE + ")", returning = "result")
     public void logAfterReturning(JoinPoint joinPoint, Object result) {
-        LOGGER.info("Method " + joinPoint.getSignature().getName() + " returned: " + result);
+        LOGGER.info("Метод " + joinPoint.getSignature().getName() + " вернул: " + result);
     }
 
     @AfterThrowing(pointcut = "execution(* " + CONTROLLER_PACKAGE + ")", throwing = "exception")
     public void logAfterThrowing(JoinPoint joinPoint, Throwable exception) {
-        LOGGER.error("Exception in method " + joinPoint.getSignature().getName() + ": " + exception.getMessage(), exception);
+        LOGGER.error("Исключение в методе " + joinPoint.getSignature().getName() + ": " + exception.getMessage(), exception);
     }
 }
