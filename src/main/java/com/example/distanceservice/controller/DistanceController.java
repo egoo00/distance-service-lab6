07bcb82fc7f104532070a7ac12179b76791025cd
egoo00 <egooo914@gmail.com>
2025-06-24 16:35:43 +0300

    Update DistanceController.java

--- src/main/java/com/example/distanceservice/controller/DistanceController.java
+++ src/main/java/com/example/distanceservice/controller/DistanceController.java
@@ -2,7 +2,7 @@ package com.example.distanceservice.controller;
 
 import com.example.distanceservice.dto.DistanceResponse;
 import com.example.distanceservice.service.DistanceService;
-import com.example.distanceservice.util.RequestCounter;
+import com.example.distanceservice.aspect.RequestCounterAspect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,62 +17,41 @@ public class DistanceController {
     private static final String API_PATH_RESET = "/reset";
 
     private final DistanceService distanceService;
-    private final RequestCounter requestCounter;
+    private final RequestCounterAspect requestCounterAspect;
 
     @Autowired
-    public DistanceController(DistanceService distanceService, RequestCounter requestCounter) {
+    public DistanceController(DistanceService distanceService, RequestCounterAspect requestCounterAspect) {
         this.distanceService = distanceService;
-        this.requestCounter = requestCounter;
+        this.requestCounterAspect = requestCounterAspect;
     }
 
     @GetMapping(API_PATH_DISTANCE)
     public DistanceResponse getDistance(@RequestParam String from, @RequestParam String to) {
-        requestCounter.incrementTotal();
         try {
             DistanceResponse response = distanceService.calculateDistance(from, to);
-            requestCounter.incrementSuccessful();
             return response;
         } catch (Exception e) {
-            requestCounter.incrementFailed();
             throw e;
         }
     }
 
     @PostMapping(API_PATH_DISTANCES_BULK)
-    public List<DistanceResponse> getBulkDistances(@RequestBody List<String[]> cityPairs) {
-        requestCounter.incrementTotal();
+    public List<DistanceResponse> getBulkDistances(@RequestBody List<CityPair> cityPairs) {
         try {
             List<DistanceResponse> responses = distanceService.calculateBulkDistances(cityPairs);
-            requestCounter.incrementSuccessful();
             return responses;
         } catch (Exception e) {
-            requestCounter.incrementFailed();
             throw e;
         }
     }
 
     @GetMapping(API_PATH_COUNTER)
     public int getRequestCount() {
-        requestCounter.incrementTotal();
-        try {
-            int count = requestCounter.getTotalRequests();
-            requestCounter.incrementSuccessful();
-            return count;
-        } catch (Exception e) {
-            requestCounter.incrementFailed();
-            throw e;
-        }
+        return requestCounterAspect.getTotalRequests();
     }
 
     @GetMapping(API_PATH_RESET)
     public void resetCounter() {
-        requestCounter.incrementTotal();
-        try {
-            requestCounter.reset();
-            requestCounter.incrementSuccessful();
-        } catch (Exception e) {
-            requestCounter.incrementFailed();
-            throw e;
-        }
+        requestCounterAspect.reset();
     }
 }
